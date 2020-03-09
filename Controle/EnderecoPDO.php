41df3381ca0a5912177f5ed49fd41862f63208cd
Anesi <daniel.o.anesi@gmail.com>
2020-03-09 09:02:06 -0300

    Editando endereco

--- Controle/EnderecoPDO.php
+++ Controle/EnderecoPDO.php
@@ -1,56 +1,40 @@
 <?php
 
-if (realpath('./index.php')) {
-    include_once './Controle/conexao.php';
-    include_once './Modelo/Endereco.php';
-} else {
-    if (realpath('../index.php')) {
-        include_once '../Controle/conexao.php';
-        include_once '../Modelo/Endereco.php';
-    } else {
-        if (realpath('../../index.php')) {
-            include_once '../../Controle/conexao.php';
-            include_once '../../Modelo/Endereco.php';
+        include_once __DIR__.'/../Controle/conexao.php';
+        include_once __DIR__.'/../Modelo/Endereco.php';
+        include_once __DIR__.'/../Controle/UsuarioPDO.php';
+        include_once __DIR__.'/../Controle/EmpregadorPDO.php';
+
+
+
+class EnderecoPDO
+{
+
+    function inserirEnderecoEmpregado()
+    {
+        $endereco = new endereco($_POST);
+        $id_empregado = $_POST['id'];
+        $pdo = conexao::getConexao();
+        $stmt = $pdo->prepare('insert into Endereco values(default , :endereco , :cep , :numero , :complemento , :estado , :cidade);');
+        $stmt->bindValue(':endereco', $endereco->getEndereco());
+        $stmt->bindValue(':cep', $endereco->getCep());
+        $stmt->bindValue(':numero', $endereco->getNumero());
+        $stmt->bindValue(':complemento', $endereco->getComplemento());
+        $stmt->bindValue(':estado', $endereco->getEstado());
+        $stmt->bindValue(':cidade', $endereco->getCidade());
+        if ($stmt->execute()) {
+            $usuarioPDO = new UsuarioPDO();
+            $usuarioPDO->relacionaEndereco($pdo->lastInsertId(), $id_empregado);
+        } else {
+            $_SESSION['toast'][] = 'Erro ao inserir endereco';
+            header("Location: ../Tela/perfilEmpregado.php");
         }
     }
-}
 
-
-class EnderecoPDO{
-    
-             /*inserir*/
-    function inserirEndereco() {
-        $endereco = new endereco($_POST);
-            $con = new conexao();
-            $pdo = $con->getConexao();
-            $stmt = $pdo->prepare('insert into Endereco values(default , :endereco , :cep , :numero , :complemento , :estado , :cidade);' );
-
-            $stmt->bindValue(':endereco', $endereco->getEndereco());    
-        
-            $stmt->bindValue(':cep', $endereco->getCep());    
-        
-            $stmt->bindValue(':numero', $endereco->getNumero());    
-        
-            $stmt->bindValue(':complemento', $endereco->getComplemento());    
-        
-            $stmt->bindValue(':estado', $endereco->getEstado());    
-        
-            $stmt->bindValue(':cidade', $endereco->getCidade());    
-        
-            if($stmt->execute()){ 
-                header('location: ../index.php?msg=enderecoInserido');
-            }else{
-                header('location: ../index.php?msg=enderecoErroInsert');
-            }
-    }
     /*inserir*/
-                
-    
+    public function selectEndereco()
+    {
 
-            
-
-    public function selectEndereco(){
-            
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco ;');
@@ -61,11 +45,10 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoId_endereco($id_endereco){
-            
+    public function selectEnderecoId_endereco($id_endereco)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where id_endereco = :id_endereco;');
@@ -77,11 +60,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoEndereco($endereco){
-            
+
+    public function selectEnderecoEndereco($endereco)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where endereco = :endereco;');
@@ -93,11 +76,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoCep($cep){
-            
+
+    public function selectEnderecoCep($cep)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where cep = :cep;');
@@ -109,11 +92,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoNumero($numero){
-            
+
+    public function selectEnderecoNumero($numero)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where numero = :numero;');
@@ -125,11 +108,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoComplemento($complemento){
-            
+
+    public function selectEnderecoComplemento($complemento)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where complemento = :complemento;');
@@ -141,11 +124,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoEstado($estado){
-            
+
+    public function selectEnderecoEstado($estado)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where estado = :estado;');
@@ -157,11 +140,11 @@ class EnderecoPDO{
             return false;
         }
     }
-    
 
-                    
-    public function selectEnderecoCidade($cidade){
-            
+
+    public function selectEnderecoCidade($cidade)
+    {
+
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('select * from endereco where cidade = :cidade;');
@@ -173,30 +156,26 @@ class EnderecoPDO{
             return false;
         }
     }
-    
- 
-    public function updateEndereco(Endereco $endereco){        
+
+
+    public function updateEndereco(Endereco $endereco)
+    {
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('update endereco set endereco = :endereco , cep = :cep , numero = :numero , complemento = :complemento , estado = :estado , cidade = :cidade where id_endereco = :id_endereco;');
         $stmt->bindValue(':endereco', $endereco->getEndereco());
-        
         $stmt->bindValue(':cep', $endereco->getCep());
-        
         $stmt->bindValue(':numero', $endereco->getNumero());
-        
         $stmt->bindValue(':complemento', $endereco->getComplemento());
-        
         $stmt->bindValue(':estado', $endereco->getEstado());
-        
         $stmt->bindValue(':cidade', $endereco->getCidade());
-        
         $stmt->bindValue(':id_endereco', $endereco->getId_endereco());
         $stmt->execute();
         return $stmt->rowCount();
-    }            
-    
-    public function deleteEndereco($definir){
+    }
+
+    public function deleteEndereco($definir)
+    {
         $con = new conexao();
         $pdo = $con->getConexao();
         $stmt = $pdo->prepare('delete from endereco where id_endereco = :definir ;');
@@ -204,24 +183,26 @@ class EnderecoPDO{
         $stmt->execute();
         return $stmt->rowCount();
     }
-    
-    public function deletar(){
+
+    public function deletar()
+    {
         $this->deleteEndereco($_GET['id']);
         header('location: ../Tela/listarEndereco.php');
     }
 
 
-
-            /*editar*/
-            function editar() {
-                $endereco = new Endereco($_POST);
-                    if($this->updateEndereco($endereco) > 0){
-                        header('location: ../index.php?msg=enderecoAlterado');
-                    } else {
-                        header('location: ../index.php?msg=enderecoErroAlterar');
-                    }
-            }
-            /*editar*/
-            /*chave*/
-            }
-                
+    /*editar*/
+    function editarEnderecoEmpregado()
+    {
+        $endereco = new Endereco($_POST);
+        if ($this->updateEndereco($endereco) > 0) {
+            $_SESSION['toast'][] = 'Endereço alterado com sucesso!';
+            header('location: ../Tela/perfilEmpregado.php');
+        } else {
+            $_SESSION['toast'][] = 'Erro ao alterar endereço!';
+            header('location: ../Tela/perfilEmpregado.php');
+        }
+    }
+    /*editar*/
+    /*chave*/
+}
