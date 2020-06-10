9eaf056758f2fd0af1636a50d0d3dbe45b837ef4
Daniel Anesi <38354920+danesi@users.noreply.github.com>
2020-06-10 15:14:19 -0300

    Update update_1_1-1_2.sql

--- Banco/update_1_1-1_2.sql
+++ Banco/update_1_1-1_2.sql
@@ -1,20 +1,3 @@
-create table nota_empregado
-(
-    id_nota_empregado int auto_increment
-        primary key,
-    id_usuario        int not null,
-    nota              int not null,
-    constraint nota_empregado_empregado
-        foreign key (id_usuario) references empregado (id_usuario)
-);
+create table nota_empregado ( id_nota_empregado int auto_increment primary key, id_empregado int not null, id_usuario int NOT null, nota int not null, constraint notaEmpregado_empregado foreign key (id_empregado) references empregado (id_usuario), constraint notaEmpregado_usuario foreign key (id_usuario) references usuario (id_usuario) )
 
-create table nota_servico
-(
-    id_nota_servico int auto_increment
-        primary key,
-    id_servico      int not null,
-    nota            int not null,
-    constraint nota_servico_servico
-        foreign key (id_servico) references servico (id_servico)
-);
-
+create table nota_servico ( id_nota_servico int auto_increment primary key, id_servico int not null, id_usuario int NOT null, nota int not null, constraint notaSservico_servico foreign key (id_servico) references servico (id_servico), constraint notaSmpregado_usuario foreign key (id_usuario) references usuario (id_usuario) )
